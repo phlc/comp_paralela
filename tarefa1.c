15da8b5b02c7f440ebf68319487d1b400e7e5bf9
phlc <pedrocarvalho@outlook.com>
2022-02-10 16:20:35 -0300

    first test ok

--- tarefa1.c
+++ tarefa1.c
@@ -12,13 +12,18 @@ int main()
 {
     int i;
 
-    #pragma omp parallel num_threads(2) // seta o número de threads em 2 
-    {
-        int tid = omp_get_thread_num(); // lê o identificador da thread 
-        for(i = 1; i <= 3; i++) 
-        {
-           printf("[PRINT1] T%d = %d \n",tid,i);
-           printf("[PRINT2] T%d = %d \n",tid,i);
-        }
-    }
+   #pragma omp parallel num_threads(2) // seta o número de threads em 2 
+   {
+      int tid = omp_get_thread_num(); // lê o identificador da thread 
+    
+      #pragma omp for ordered
+      for(i = 1; i <= 3; i++) 
+      {
+         #pragma omp ordered
+         {
+            printf("[PRINT1] T%d = %d \n",tid,i);
+            printf("[PRINT2] T%d = %d \n",tid,i); 
+         }
+       }
+   }
 }
