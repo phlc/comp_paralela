ed53c4825cf5e4fbdf7c7f169441816fd887e66e
phlc <pedrocarvalho@outlook.com>
2022-05-19 12:06:20 -0300

    tarefa28.c otimizada

--- tarefa28.c
+++ tarefa28.c
@@ -32,19 +32,23 @@ void removeMultipleOf3() {
 }
 
 void removeMultipleOf5() {  	
-   int number;
+   int number, i=0;
    MPI_Status Status; 
 	
    while (1)  {
      MPI_Recv(&number,1,MPI_INT,0,MPI_ANY_TAG,MPI_COMM_WORLD,&Status);
      if (Status.MPI_TAG == END_MSG) break;
      if (number % 5 > 0) {
+      int proximo = (i % (size -2)+2);
+      i++; 
        // enviar para o próximo estágio
-      MPI_Send(&number,1,MPI_INT,2,PIPE_MSG,MPI_COMM_WORLD);
+      MPI_Send(&number,1,MPI_INT,proximo,PIPE_MSG,MPI_COMM_WORLD);
      }
    }
    // enviar mensagem de finalização
-   MPI_Send(&number,1,MPI_INT,2,END_MSG,MPI_COMM_WORLD);
+   for(i=2;i<size; i++){
+      MPI_Send(&number,1,MPI_INT,i,END_MSG,MPI_COMM_WORLD);
+   }
 }
 
 void countOnlyPrimes() {
@@ -52,14 +56,14 @@ void countOnlyPrimes() {
    int number, primeCount, i, isComposite;
    MPI_Status Status; 
 
-   primeCount = 3;  
+   primeCount = 0;  
 
    while (1)  {
       // receber mensagem do estágio anterior
       // sair do loop se fim
      MPI_Recv(&number,1,MPI_INT,1,MPI_ANY_TAG,MPI_COMM_WORLD,&Status);
      if (Status.MPI_TAG == END_MSG) break;
- 
+      
       isComposite = 0;
       for (i = 7; i*i <= number; i += 2)
         if (number % i == 0)  {
@@ -70,7 +74,20 @@ void countOnlyPrimes() {
 	  if (!isComposite) primeCount++;  
    }
 
-   printf("number of primes = %d\n",primeCount);
+    MPI_Send(&primeCount,1,MPI_INT,0,END_MSG,MPI_COMM_WORLD);
+ 
+}
+
+void reduce(){
+   int i, subtotal = 0;
+   int primeCount = 3;
+   MPI_Status Status;
+   for (int i=2; i<size; i++){
+      MPI_Recv(&subtotal,1,MPI_INT,i,MPI_ANY_TAG,MPI_COMM_WORLD,&Status);
+      primeCount += subtotal;
+
+}
+   printf("number of primes: %i\n", primeCount); 
 }
 
 void main(int argc, char **argv) {  
@@ -83,12 +100,14 @@ void main(int argc, char **argv) {
 
    switch (rank)  {
       case 0:  removeMultipleOf3();
+               reduce();
 	  break;
       
 	  case 1:  removeMultipleOf5();
       break;
       
-	  case 2:  countOnlyPrimes();
+      default: countOnlyPrimes();
+      break;
    };
 
    MPI_Finalize();
